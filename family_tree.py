01a0f735e0d53a40089d250970f97b39a4a11763
Joey <sharpshooter30@gmail.com>
2016-04-02 15:49:22 -0600

    working on the parent and orders

--- family_tree.py
+++ family_tree.py
@@ -28,17 +28,41 @@ class family_tree:
 
     """ Return a preorder list """
 
-    def preorder(self):
+    def pre_order(self):
+        results = []
+
+        if self.__value is None:
+            return results
+
+        results += [self]
+
+        if self.__left:
+            results += self.pre_order(self.__left)
+
+        if self.__right:
+            results += self.pre_order(self.__right)
         pass
 
-    """ Return a inorder list """
+    """ Return a in_order list """
 
-    def inorder(self):
-        pass
+    def in_order(self):
+        value = self.__value
+        results = []
 
-    """ Return a postorder list """
+        if value is None:
+            return results
 
-    def postorder(self):
+        if self.__left:
+            results += self.in_order(value)
+
+        # results += [self]
+
+        if self.__right:
+            results += self.in_order(value)
+
+    """ Return a post_order list """
+
+    def post_order(self):
         pass
 
     def __str__(self):
@@ -90,7 +114,37 @@ class family_tree:
     """ Given a value, return the name of that node's parent """
 
     def find_parent(self, value):
-        pass
+        print(self.__value)
+        print(self.__name)
+        print(self.__left)
+        print(self.__parent)
+        print(value)
+        if self.__value == value:
+
+            return self.__name
+
+        if self.__parent > value:
+
+            print(value)
+            print (self.__name)
+            print(self.__parent)
+            if self.__left:
+
+                return self.__left.find_parent(value)
+
+            else:
+
+                raise LookupError
+
+        if self.__parent < value:
+
+            if self.__right:
+
+                return self.__right.find_parent(value)
+
+            else:
+
+                raise LookupError
 
     """ Given a value, return the name of that node's grand parent """
 
@@ -160,9 +214,18 @@ class test_family_tree(unittest.TestCase):
     """ Write your own tests for inorder etc. here
     used from the discussion board"""
 
-    def test_inorder(self):
-        self.assertEquals((self.tree.inorder()), [(10, 'Herb'), (20, 'Grandpa'),
-                                                  (25, 'Bart'), (30, 'Homer'), (35, 'Lisa')])
+    def test_pre_order(self):
+        self.assertEqual((self.tree.pre_order()),
+                         [(20, 'Grandpa'), (10, 'Herb'), (30, 'Homer'), (25, 'Bart'), (35, 'Lisa')])
+
+    def test_post_order(self):
+        self.assertEqual((self.tree.post_order()),
+                         [(10, 'Herb'), (25, 'Bart'), (35, 'Lisa'), (30, 'Homer'),  (20, 'Grandpa')])
+
+    def test_in_order(self):
+        self.assertEquals((self.tree.in_order()), [(10, 'Herb'), (20, 'Grandpa'),
+                                                   (25, 'Bart'), (30, 'Homer'), (35, 'Lisa')])
+
 
 if '__main__' == __name__:
     """ Read a file with lines of '# name'. Add each to a
