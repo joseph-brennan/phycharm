d40dd9ad62b7fd583fa4dde63de3ed6be7368e0b
Joey <sharpshooter30@gmail.com>
2016-04-27 12:20:26 -0600

    first attempt at writing solve function

--- eight_ queens.py
+++ eight_ queens.py
@@ -1,9 +1,13 @@
+from __future__ import print_function
 import unittest
+
+track = False
 """ This method assumes the solution is a list of tuples such as:
     [(0, 1), (1, 3), (2, 0), (3, 2)] """
 
 
 def print_board(solution):
+
     length = len(solution)
 
     print("for:", length)
@@ -12,54 +16,67 @@ def print_board(solution):
 
     if not solution:
 
-        print ("no solution found")
+        print("no solution found")
     else:
         for i in range(length):
 
             for j in range(length):
 
-                """if (i, j) in solution:
+                if (i, j) in solution:
 
-                    print("Q", end = "")
+                    print("Q", end="")
                 else:
 
-                    print(".", end = "")"""
+                    print(".", end="")
             print()
 
     print('-' * length)
+
 
 ''' Given the location of two queens, find if they are safe
     from each other. '''
 
 
 def safe((x1, y1), (x2, y2)):
-
     if x1 == x2:
         return False
 
     if y1 == y2:
         return False
 
-    if abs(x2-x1) == abs(y2-y1):
+    if abs(x2 - x1) == abs(y2 - y1):
         return False
 
     return True
-'''
-solve_queens(row)
-    if the row is greater than the size of the board, we're done
-
-    go through the columns in this row
-        go through all the already placed queens and see if
-            placing a new queen at (row, column) is safe
-
-        if it is
-            place it at (row, column)
-            if not solve_queens(row+1)
-                remove row and column
-'''
 
 
-class test_eight_queens (unittest.TestCase):
+''' solve_queens(row)'''
+
+
+def solve_queen(row, columns):
+    solution = []
+    """if the row is greater than the size of the board, we're done"""
+    if row > len(solution):
+        return solution
+
+    """go through the columns in this row"""
+    for columns in row:
+        """go through all the already placed queens and see if
+            placing a new queen at (row, column) is safe"""
+
+        if safe(queen) is True:
+            solution.append(queen)
+
+            """if it is
+             place it at (row, column)"""
+
+        else:
+            solve_queen(row + 1)
+
+            """if not solve_queens(row+1)
+                remove row and column"""
+
+
+class test_eight_queens(unittest.TestCase):
     def test_safe_empty(self):
         self.assertEqual(safe((None, None), (None, None)), False)
-
